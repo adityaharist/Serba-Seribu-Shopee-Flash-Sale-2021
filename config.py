e828cd3f11b3036a4eea357f7177edd19f456aad
Aditya Harist <jumphigher19@gmail.com>
2021-07-07 21:25:59 +0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+
+# pin_number ="969655"
+# link_produk ="https://shopee.co.id/-COD-Micropack-Powerbank-10000-mAh-Fast-Charging-for-Oppo-VIVO-Samsung-Xiaomi-Redmi-iPhone-i.98362995.7245279462"
+# cookiee = 'b0dEEsaI6h/BuT7Zfnad+M5H9fLdTNzV+PrCVm5s+UVZQfQoRrnXw7NS5SZXycgkzk3m8aqHRf+fuZEkUGnqmvCH7ak5AQFxHC4PIoIKqpwZ9S1k4HuPbTmZ89XcNnW9Sx5h+HUmilsVcBOCcVkR2ofoC5fzz0lk3mFcwYfW3sY='
+
+pin_number = "pinShopeepayElu"
+link_produk = "https://shopee.co.id/Apple-iPhone-12-128GB-Blue-i.241308147.8301975916"
+cookiee = "4+O+55VuyYqYnuHf5fMBZ/3SvMlBZ9ZmB+e3m7f9HXjRY+qZsBwo9BA8Tuox66waMdp1txUaHU1/o+6/3fu4S0hUTD9AYRKaT4biaVNDJBC5Moqn9qEoHIkop0gZmcph4C3chHQiv2mJxtmOSwq6oCf8KaeycGQ1gqb26OcKeL0="
